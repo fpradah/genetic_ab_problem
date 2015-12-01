7126e6b078233f21abd255ba2e1d6cd7a02e1873
Alberto Galera <galerajimenez@gmail.com>
2015-12-01 09:05:35 +0100

    pep8 and code styling

--- src/Cromosoma.py
+++ src/Cromosoma.py
@@ -1,50 +1,69 @@
 from random import *
-import sys
 
-class Cromosoma :
-	"""Cromosoma Base de la solucion"""
-	def __init__(self,genes,pares):
-		self.genes = genes
-		self.pares = pares
-		self.cromosoma = self.createRandom()
-		self.totalPares = self.getPares()
-		self.puntuacion = self.getPuntuacion()
-	def getCromosoma(self):
-		return cromosoma
-	def createRandom(self):
-		return [randint(0,1) 
-		for b in range(1,self.genes+1)]
-	def getPuntuacion(self):
-		total_pares = self.getPares() 
-		if self.pares == 0 and total_pares == 0 : return 1
-		if self.pares == 0 and total_pares == 1 : return 0.5
-		if self.pares == 0 : return float(total_pares)/1
-		return float(total_pares)/float(self.pares)
-	def cruzar(self,pareja):
-		# Escogemos el pocentaje de la pareja
-		total_actual = int(self.genes * 0.6)
-		# Escogemos los genes de la pareja
-		for gen in range(total_actual,self.genes-1) :
-			self.cromosoma[gen] = pareja[gen]
-	def mutar(self,porcentaje_mutacion):
-		total_mutacion = int(len(self.cromosoma) * porcentaje_mutacion)
-		for i in range(0,total_mutacion) :
-			# Elegimo una posicion random de gen
-			posicion_gen = randint(0,self.genes-1)
-			# La remplazamos por un valor aleatorio
-			self.cromosoma[posicion_gen] = randint(0,1)
-	def getPares(self):
-		self.totalPares = 0
-		for i in range(0,len(self.cromosoma)):
-			if (self.cromosoma[i]==1):
-				continue
-			for j in range(i,len(self.cromosoma)):
-				if (self.cromosoma[i]<self.cromosoma[j]):
-					self.totalPares = self.totalPares + 1
-		return self.totalPares
-	def isSolution(self):
-		return self.puntuacion == 1
-	def isBestSolution(self):
-		return self.puntuacion > 0.8 or self.puntuacion < 1.2
-	def toString(self):
-		print self.cromosoma,"Puntuacion",self.puntuacion,"Pares",self.pares,"Total Pares",self.getPares()
+
+class Cromosoma:
+
+    """Cromosoma Base de la solucion"""
+
+    def __init__(self, genes, pares):
+        self.genes = genes
+        self.pares = pares
+        self.cromosoma = self.createRandom()
+        self.totalPares = self.getPares()
+        self.puntuacion = self.getPuntuacion()
+
+    def getCromosoma(self):
+        return cromosoma
+
+    def createRandom(self):
+        return [randint(0, 1)
+                for b in range(1, self.genes + 1)]
+
+    def getPuntuacion(self):
+        total_pares = self.getPares()
+        if self.pares == 0 and total_pares == 0:
+            return 1
+        if self.pares == 0 and total_pares == 1:
+            return 0.5
+        if self.pares == 0:
+            return float(total_pares) / 1
+        return float(total_pares) / float(self.pares)
+
+    def cruzar(self, pareja):
+        # Escogemos el pocentaje de la pareja
+        total_actual = int(self.genes * 0.6)
+        # Escogemos los genes de la pareja
+        for gen in range(total_actual, self.genes - 1):
+            self.cromosoma[gen] = pareja[gen]
+
+    def mutar(self, porcentaje_mutacion):
+        total_mutacion = int(len(self.cromosoma) * porcentaje_mutacion)
+        for i in range(0, total_mutacion):
+            # Elegimo una posicion random de gen
+            posicion_gen = randint(0, self.genes - 1)
+            # La remplazamos por un valor aleatorio
+            self.cromosoma[posicion_gen] = randint(0, 1)
+
+    def getPares(self):
+        self.totalPares = 0
+        for i in range(len(self.cromosoma)):
+            if (self.cromosoma[i] == 1):
+                continue
+            for j in range(i, len(self.cromosoma)):
+                if (self.cromosoma[i] < self.cromosoma[j]):
+                    self.totalPares = self.totalPares + 1
+        return self.totalPares
+
+    def isSolution(self):
+        return self.puntuacion == 1
+
+    def isBestSolution(self):
+        return self.puntuacion > 0.8 or self.puntuacion < 1.2
+
+    def toString(self):
+
+        r = "%s Puntuacion %s Pares %s Total %s Pares" % (self.cromosoma,
+                                                          self.puntuacion,
+                                                          self.pares,
+                                                          self.getPares())
+        return r
--- src/run.py
+++ src/run.py
@@ -1,35 +1,35 @@
 from random import *
 import sys
-import Cromosoma
 import Poblacion
 
-genes = int(sys.argv[1])
-pares = int(sys.argv[2])
-tamano_poblacion = genes*5
-maxima_generacion = 1000*genes
+genes, pares = int(sys.argv[1]), int(sys.argv[2])
+tamano_poblacion = genes * 5
+maxima_generacion = 1000 * genes
 solucion = []
-"""Creamos la primera generacion"""  
-poblacion = Poblacion.Poblacion(tamano_poblacion,genes,0,pares)
+"""Creamos la primera generacion"""
+poblacion = Poblacion.Poblacion(tamano_poblacion, genes, 0, pares)
 
-seguir = 1
-while seguir and maxima_generacion > poblacion.generacion :
-	"""Buscamos si existe solucion en la poblacion actual"""
-	print "GENERACION : "
-	poblacion.toString()
-	solucion = poblacion.getPerfectCromosoma()
-	if len(solucion) > 0 : 
-		seguir = 0
-		break
-	"""Creamos la siguiente generacion"""  
-	print "###########################"
-	print "Nueva GENERACION",poblacion.generacion
-	print "###########################"
-	poblacion.newGeneration()
+seguir = True
+while seguir and maxima_generacion > poblacion.generacion:
+    """Buscamos si existe solucion en la poblacion actual"""
+    print "GENERACION : "
+    print poblacion.toString()
+    solucion = poblacion.getPerfectCromosoma()
+    if len(solucion) > 0:
+        seguir = False
+        break
+    """Creamos la siguiente generacion"""
+    print "###########################"
+    print "Nueva GENERACION", poblacion.generacion
+    print "###########################"
+    poblacion.newGeneration()
 
 solucion_text = ""
-for i in range(0,len(solucion)) :
-	if solucion[i] == 1 : solucion_text += 'B'
-	else : solucion_text += 'A'
+for i in range(0, len(solucion)):
+    if solucion[i] == 1:
+        solucion_text += 'B'
+    else:
+        solucion_text += 'A'
 print "Solucion "
 print "----------"
 print solucion_text
